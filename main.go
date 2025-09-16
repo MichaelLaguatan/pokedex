310d5937302d73d4545f7f6661d883663ab7e0f5
Michael Laguatan <michael.laguatan03@gmail.com>
2025-09-16 04:04:30 -0600

    Reworked file structure and added command support for help and exit

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := cleanInput(scanner.Text())
-
-	}
-}
-
-func cleanInput(text string) []string {
-	return strings.Fields(strings.ToLower(text))
+	startRepl()
 }
---
+++ repl.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := cleanInput(scanner.Text())
+		if command, ok := getCommands()[input[0]]; ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Printf("Error exiting pokedex: %v", err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+	}
+}
+
+func cleanInput(text string) []string {
+	return strings.Fields(strings.ToLower(text))
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
